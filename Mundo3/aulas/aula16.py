4ce93b4ba3a26e93722774cd7a20aeabf057932c
renancodepro <alvesrenan964@gmail.com>
2022-05-02 10:17:16 -0300

    python

--- Mundo3/aulas/aula16.py
+++ Mundo3/aulas/aula16.py
@@ -1,1 +1,15 @@
-lanche = ('Hambúrger', 'Suco', 'Pizza', 'Pudim')
+'''lanche = 'Hambúrger', 'Suco', 'Pizza', 'Pudim'
+print(lanche[1:3])
+
+lanche = 'Hambúrger', 'Suco', 'Pizza', 'Pudim'
+print(lanche[-2])'''
+
+# Tuplas são imutáveis
+
+'''lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')'''
+
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita'
+print(len(lanche))
