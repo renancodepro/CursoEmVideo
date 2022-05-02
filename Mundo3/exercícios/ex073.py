be14da8cb0cb8c54e07c2fa2ff23aee1cedcea2b
renancodepro <alvesrenan964@gmail.com>
2022-05-02 19:08:47 -0300

    Tabela do Brasileirão

--- Mundo3/exercícios/ex073.py
+++ Mundo3/exercícios/ex073.py
@@ -4,14 +4,15 @@ times = ('Corinthians', 'Bragatino', 'Atlético-MG', 'Santos',
          'Ceará SC', 'Athetico-PR', 'Atlético-GO', 'Goiás', 'Juventude',
          'Fortaleza')
 print('=' * 40)
-print(f'Lista de times do Brasileirão: {times}')
+print(f'Lista de times do Brasileirão: {times}')  # mostra a lista dos times
+'''for t in times:
+    print(t)
+print('=' * 40)'''  # escreve a lista em coluna
+print(f'Os 5 primeiros são: {times[0:5]}')  # mostra os 5 primeiros da tupla
 print('=' * 40)
-print(f'Os 5 primeiros são: {times[0:5]}')
+print(f'Os 4 últimos são: {times[-4:]}')  # mostra os 4 últimos da tupla
 print('=' * 40)
-print(f'Os 4 últimos são: {times[-1:-5:-1]}')
+print(f'Times em ordem alfabética: {sorted(times)}')  # tupla em ordem alfabé.
 print('=' * 40)
-print(f'Times em ordem alfabética: {sorted(times)}')
-print('=' * 40)
-print('O Corinthians está em ', end='')
-print(times.index('Corinthians') + 1, end='')
-print('° Lugar no ranking do Brasileirão serie A.')
+print(f'O Corinthians está em {times.index("Corinthians") + 1}'
+      'ª no Ranking do Brasileirão serie A.')  # localiza o termo dentro da t.
