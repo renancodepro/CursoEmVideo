61e55eaa04b5d828f8daf0bc5a05f64e25c98971
renancodepro <alvesrenan964@gmail.com>
2022-05-02 14:52:15 -0300

    PYTHON

--- Mundo3/exercícios/ex072.py
+++ Mundo3/exercícios/ex072.py
@@ -1,7 +1,22 @@
-extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+'''extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+           'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze',
+           'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 num = -1
 while num < 0 or num > 20:
     num = int(input('Digite um número entre 0 e 20: '))
     if num < 0 or num > 20:
         num = int(input('Número inválido! Digite novamente: '))
-print(f'Você digitou o número {extenso[num]}')
+print(f'Você digitou o número {extenso[num]}')'''
+
+extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete',
+           'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze',
+           'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+resposta = 'S'
+while resposta in 'Ss':
+    while True:
+        núm = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= núm <= 20:
+            break
+        print('Tente novamente. ', end='')
+    print(f'Você digitou o número {extenso[núm]}')
+    resposta = str(input('Quer continuar? [S/N]: '))
